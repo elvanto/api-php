f00c5fc4356ce7d69e75ec5e1716d4dd1f71386a
Ben Sinclair <me@bensinclair.co>
2013-09-15 17:12:11 +0100

    Added new authentication and updated readme.

--- README.md
+++ README.md
@@ -1,22 +1,111 @@
-elvanto API PHP Library
-=======
+# elvanto API PHP Library
 
-This library is all set to go with version 1 of our API. Just create the object and call a method like this:
+This library is all set to go with version 1 of the <a href="http://elvanto.com/api/" target="_blank">elvanto API</a>.
 
-    $elvanto = new elvanto_API('your api key');
-    $people = $elvanto->call('people.getAll', array('archived' => 'no'));
+## Authenticating
 
-Documentation
-=======
+The elvanto API supports authentication using either <a href="http://elvanto.com/api/getting-started/#oauth" target="_blank">OAuth 2</a> or an <a href="http://elvanto.com/api/getting-started/#api_key" target="_blank">API key</a>.
 
-Documentation on the elvanto API can be found on the <a href="http://elvanto.com/api/" target="_blank">API website</a>.
+### Using OAuth 2
 
-Updates
-=======
+This library provides functionality to help you obtain an Access Token and Refresh token. The first thing your application should do is redirect your user to the elvanto authorization URL where they will have the opportunity to approve your application to access their elvanto account. You can get this authorization URL by using the `authorize_url()` method, like so:
+
+```php
+require_once('elvanto_API.php');
+
+$elvanto = new elvanto_API();
+
+$authorize_url = $elvanto->authorize_url(
+	'Client ID for your application',
+    'Redirect URI for your application',
+    'The permission level your application requires',
+    'Optional state data to be included'
+);
+// Redirect your users to $authorize_url.
+```
+
+If your user approves your application, they will then be redirected to the `redirect_uri` you specified, which will include a `code` parameter, and optionally a `state` parameter in the query string. Your application should implement a handler which can exchange the code passed to it for an access token, using `exchange_token()` like so:
+
+```php
+require_once('elvanto_API.php');
+
+$elvanto = new elvanto_API();
+
+$result = $elvanto->exchange_token(
+    'Client ID for your application',
+    'Client Secret for your application',
+    'Redirect URI for your application',
+    'A unique code for your user' // Get the code parameter from the query string.
+);
+
+$access_token = $result->access_token;
+$expires_in = $result->expires_in;
+$refresh_token = $result->refresh_token;
+// Save $access_token, $expires_in and $refresh_token.
+```
+
+At this point you have an access token and refresh token for your user which you should store somewhere convenient so that your application can look up these values when your user wants to make future elvanto API calls.
+
+Once you have an access token and refresh token for your user, you can authenticate and make further API calls like so:
+
+```php
+require_once('elvanto_API.php');
+
+$auth_details = array(
+    'access_token' => 'your access token',
+    'refresh_token' => 'your refresh token'
+);
+$elvanto = new elvanto_API($auth_details);
+
+$results = $elvanto->call('people/getAll');
+var_dump($results);
+```
+
+All OAuth tokens have an expiry time, and can be renewed with a corresponding refresh token. If your access token expires when attempting to make an API call, you will receive an error response, so your code should handle this. Here's an example of how you could do this:
+
+```php
+require_once('elvanto_API.php');
+
+$auth_details = array(
+    'access_token' => 'your access token',
+    'refresh_token' => 'your refresh token'
+);
+$elvanto = new elvanto_API($auth_details);
+
+$results = $elvanto->call('people/getAll');
+if (isset($results->error)) {
+    // If you receive '121: Expired OAuth Token', refresh the access token.
+    if ($results->error->code == 121) {
+        list($new_access_token, $new_expires_in, $new_refresh_token) =
+            $elvanto->refresh_token();
+        // Save $new_access_token, $new_expires_in, and $new_refresh_token.
+    }
+    // Make the call again.
+    $results = $elvanto->call('people/getAll');
+}
+var_dump($results);
+```
+
+### Using an API key
+
+```php
+require_once('elvanto_API.php');
+
+$auth_details = array('api_key' => 'your API Key');
+$elvanto = new elvanto_API($auth_details);
+
+$results = $elvanto->call('people/getAll');
+var_dump($results);
+```
+
+## Documentation
+
+Documentation can be found on the <a href="http://elvanto.com/api/" target="_blank">elvanto API website</a>.
+
+## Updates
 
 Follow our <a href="http://twitter.com/elvantoAPI" target="_blank">Twitter</a> to keep up-to-date with changes in the API.
 
-Support
-=======
+## Support
 
-If you come across any bugs, or have any suggestions, please contact us <a href="http://elvanto.com/contact/" target="_blank">via our website</a>.
+If you come across any bugs, or have any suggestions, please <a href="https://elvanto.com/forums/forum/3/api-developers/" target="_blank">post in the forum</a> or contact us <a href="http://elvanto.com/contact/" target="_blank">via our website</a>.
--- elvanto_API.php
+++ elvanto_API.php
@@ -1,51 +1,160 @@
 <?php
-
 /**
 * Super-simple elvanto API v1 wrapper
 *
 * Requires curl
 *
-* @version 1.0
+* @version 1.0.0
 */
+
 class elvanto_API {
 
-	private $api_key;
-	private $api_endpoint = 'https://api.elvanto.com/v1/';
+	const API_ENDPOINT = 'http://api.elvanto.com/v1';
+	const OAUTH_URL = 'http://api.elvanto.com/oauth';
+	const OAUTH_TOKEN_URL = 'http://api.elvanto.com/oauth/token';
+
+	private $auth_details;
 
 	/**
-	* Create a new instance
-	* @param string $api_key Your elvanto API key
+	* Constructor
+	*
+	* @param $auth_details array Authentication details to use for API calls.
+	*		This array must take one of the following forms:
+	*
+	*		If using OAuth to authenticate:
+	*		array(
+	*		 'access_token' => 'your access token',
+	*		 'refresh_token' => 'your refresh token')
+	*
+	*		Or if using an API key:
+	*		array('api_key' => 'your api key')
+	*
+	*		When using authorize_url and exchange_token functions, the
+	*		array can be left empty.
 	*/
-	function __construct($api_key) {
-		$this->api_key = $api_key;
+	public function __construct($auth_details = array()) {
+		$this->auth_details = $auth_details;
 	}
 
 	/**
-	* Call an API method. Every request needs the API key, so that is added automatically.
-	* @param string $method The API method to call.
-	* @param array $args An array of parameters to pass to the method.
-	* @return array Associative array of json decoded API response.
+	* Call a method.
+	*
+	* @param string $method The name of the method to call.
+	* @param array $params The parameters to pass to the method.
+	* @return array The response from the API method.
+	* @access public
 	*/
-	public function call($method, $args = array()) {
+	public function call($method, $params = array()) {
+		return $this->_request($method, $params);
+	}
 
-		$args['apikey'] = $this->api_key;
+	/**
+	* Get the authorization URL for your application.
+	*
+	* @param $client_id int The Client ID of your registered OAuth application.
+	* @param $redirect_uri string The Redirect URI of your registered OAuth application.
+	* @param $scope string The comma-separated permission scope your application requires.
+	* @param $state string Optional state data to be included in the URL.
+	* @return string The authorization URL to which users of your application should be redirected.
+	* @access public
+	**/
+	public function authorize_url($client_id, $redirect_uri, $scope = 'AdministerAccount', $state = false) {
+		$qs = 'type=web_server';
+		$qs .= '&client_id=' . urlencode($client_id);
+		$qs .= '&redirect_uri=' . urlencode($redirect_uri);
+		$qs .= '&scope=' . urlencode($scope);
+		if ($state)
+			$qs .= '&state=' . urlencode($state);
+		return self::OAUTH_URL . '?' . $qs;
+	}
 
-		$url = $this->api_endpoint . $method . '.json';
+	/**
+	* Exchange a provided OAuth code for an OAuth access token, 'expires in'
+	* value and refresh token.
+	*
+	* @param $client_id int The Client ID of your registered OAuth application.
+	* @param $client_secret string The Client Secret of your registered OAuth application.
+	* @param $redirect_uri string The Redirect URI of your registered OAuth application.
+	* @param $code string The unique OAuth code to be exchanged for an access token.
+	* @return A successful response will be an array of the form:
+	* 	array(
+	*		'access_token' => The access token to use for API calls
+	*		'expires_in' => The number of seconds until this access token expires
+	*		'refresh_token' => The refresh token to refresh the access token once it expires
+	* 	)
+	* @access public
+	*/
+	public function exchange_token($client_id, $client_secret, $redirect_uri, $code = '') {
+		$params = array(
+			'grant_type' => 'authorization_code',
+			'client_id' => $client_id,
+			'client_secret' => $client_secret,
+			'redirect_uri' => $redirect_uri,
+			'code' => $code
+		);
+		return $this->_request('oauth_exchange_token', $params, self::OAUTH_TOKEN_URL);
+	}
 
+	/**
+	* Refresh the current OAuth token using the current refresh token.
+	*
+	* @access public
+	*/
+	public function refresh_token() {
+		if (!isset($this->auth_details['refresh_token']))
+			trigger_error('Error refreshing token. There is no refresh token set on this object.', E_USER_ERROR);
+		$params = array(
+			'grant_type' => 'refresh_token',
+			'refresh_token' => $this->auth_details['refresh_token']
+		);
+		return $this->_request('oauth_exchange_token', $params, self::OAUTH_TOKEN_URL);
+	}
+
+	/**
+	* Call a method.
+	*
+	* @param string $method The name of the method to call.
+	* @param array $params The parameters to pass to the method.
+	* @param string $url The URL to post to.
+	* @return array The response from the API method.
+	*
+	* @access private
+	*/
+	private function _request($method, $params = array(), $url = self::API_ENDPOINT) {
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'elvanto_API-PHP/1.0.0');
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
-		$result = curl_exec($ch);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+		// Exchange or refresh a token
+		if ($method === 'oauth_exchange_token' || $method === 'oauth_refresh_token') {
+			$headers = array('Content-Type: application/x-www-form-urlencoded');
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+		// Call an API method
+		} else {
+			$url = self::API_ENDPOINT . '/' . $method . '.json';
+			$params['grant_type'] = 'client_credentials';
+			$headers = array('Content-Type: application/json');
+			if (isset($this->auth_details['access_token'])) { // Authenticating using OAuth
+				$headers[] = 'Authorization: Bearer ' . $this->auth_details['access_token'];
+			} else if (isset($this->auth_details['api_key'])) { // Authenticating using an API key
+				curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+				curl_setopt($ch, CURLOPT_USERPWD, $this->auth_details['api_key'] . ':nopass');
+			}
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+		}
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$response = curl_exec($ch);
+		if (curl_error($ch)) {
+			trigger_error('API call to ' . $url . ' failed: ' . curl_error($ch), E_USER_ERROR);
+			return false;
+		}
 		curl_close($ch);
-
-		return $result ? json_decode($result, true) : false;
-
+		return $response ? json_decode($response) : false;
 	}
 
 }
-
-?>
